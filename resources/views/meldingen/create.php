1fd2f5444a5a4db7ef962e2c99412e8b72906a66
DavyWDK <77610877+DavyWDK@users.noreply.github.com>
2026-02-11 09:30:04 +0100

    Update create.php

--- resources/views/meldingen/create.php
+++ resources/views/meldingen/create.php
@@ -32,7 +32,6 @@
                     <option value="overig">Overig</option>
                 </select>
             </div>
-            
             <div class="form-group">
                 <label for="capaciteit">Capaciteit p/uur:</label>
                 <input type="number" min="0" name="capaciteit" id="capaciteit" class="form-input">
