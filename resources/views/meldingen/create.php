6ac6c6e1863562b69665074a3841bd380a825601
DavyWDK <77610877+DavyWDK@users.noreply.github.com>
2026-02-11 09:29:42 +0100

    Update create.php

--- resources/views/meldingen/create.php
+++ resources/views/meldingen/create.php
@@ -22,8 +22,17 @@
             </div>
             <div class="form-group">
                 <label for="type">Type</label>
-                <!-- hier komt een dropdown -->
+                <select name="type" id="type">
+                    <option value="achtbaan">Achtbaan</option>
+                    <option value="draaiend">Draaiend</option>
+                    <option value="kinder">Kinder</option>
+                    <option value="horeca">Horeca</option>
+                    <option value="show">Show</option>
+                    <option value="water">Water</option>
+                    <option value="overig">Overig</option>
+                </select>
             </div>
+            
             <div class="form-group">
                 <label for="capaciteit">Capaciteit p/uur:</label>
                 <input type="number" min="0" name="capaciteit" id="capaciteit" class="form-input">
