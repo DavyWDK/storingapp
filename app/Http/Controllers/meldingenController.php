668983a86be753132ae95e5c0d1835818f998b1f
DavyWDK <77610877+DavyWDK@users.noreply.github.com>
2026-02-11 09:49:14 +0100

    Update meldingenController.php

--- app/Http/Controllers/meldingenController.php
+++ app/Http/Controllers/meldingenController.php
@@ -6,12 +6,13 @@ $type = $_POST['type'];
 $capaciteit = $_POST['capaciteit'];
 $prioriteit = isset($_POST['prioriteit']) ? true : false;
 $melder = $_POST['melder'];
+$overige_info = $_POST['overige_info'];
 
 //1. Verbinding
 require_once '../../../config/conn.php';
 
 //2. Query
-$query = "INSERT INTO meldingen (attractie, type, capaciteit, melder) VALUES (:attractie, :type, :capaciteit, :melder)";
+$query = "INSERT INTO meldingen (attractie, type, capaciteit, prioriteit, melder, overige_info) VALUES (:attractie, :type, :capaciteit, :prioriteit, :melder, :overige_info)";
 
 //3. Prepare
 $statement = $conn->prepare($query);   
@@ -21,7 +22,9 @@ $statement->execute([
     ':attractie' => $attractie,
     ':type' => $type,
     ':capaciteit' => $capaciteit,
-    ':melder' => $melder
+    ':prioriteit' => $prioriteit,
+    ':melder' => $melder,
+    ':overige_info' => $overige_info
 ]);
 
 //5. Redirect
